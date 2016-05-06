9df919706fbb1c397bff47a88736083c81d1fb8b
Joan Domingo <joands@gmail.com>
2016-05-06 18:07:50 +0200

    first commit

---
+++ Sample/MainActivity.cs
@@ -0,0 +1,75 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using Com.Payleven.Payment.Api;
+using System;
+using Java.Util;
+
+namespace Sample
+{
+	[Activity (Label = "PointPayAPI-Xamarin", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		string apiKey = "6838f633caa8472285233e996152d29f";
+
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			//Set up Point Pay API
+			PaylevenApi.Configure(apiKey);
+
+			// Get our views from the layout resource
+			Button startPaymentButton = FindViewById<Button> (Resource.Id.button_start_payment);
+			EditText descriptionEditText = FindViewById<EditText> (Resource.Id.editText_description);
+			EditText amountEditText = FindViewById<EditText> (Resource.Id.editText_amount);
+			RadioGroup currencyRadioGroup = FindViewById<RadioGroup> (Resource.Id.currency_radiogroup);
+
+			// attach event to the button
+			startPaymentButton.Click += delegate {
+				try {
+					//start payment
+					int amount = Int32.Parse(amountEditText.Text);
+					string description = descriptionEditText.Text;
+
+					RadioButton checkedButton = FindViewById<RadioButton> (currencyRadioGroup.CheckedRadioButtonId);
+					Currency currency = Currency.GetInstance(checkedButton.Text);
+
+					TransactionRequestBuilder builder = new TransactionRequestBuilder(amount, currency);
+					builder.SetDescription(description);
+
+					TransactionRequest transactionRequest = builder.CreateTransactionRequest();
+
+
+					Guid g = Guid.NewGuid();
+					string orderId = Convert.ToBase64String(g.ToByteArray());
+					orderId = orderId.Replace("=","");
+					orderId = orderId.Replace("+","");
+
+					PaylevenApi.InitiatePayment(this, orderId, transactionRequest);
+					
+				} catch (Exception e) {
+					//handle error
+				}
+			};
+		}
+
+		protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+		{
+			base.OnActivityResult(requestCode, resultCode, data);
+			//handle response in ResultActivity
+			Intent i = new Intent(this, typeof(ResultActivity));
+			if (data != null) {
+				i.PutExtra("result", data.Extras);
+			}
+			i.PutExtra("request_code", requestCode);
+			this.StartActivity(i);
+		}
+	}
+}
+
+
---
+++ Sample/ResultActivity.cs
@@ -0,0 +1,72 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using Com.Payleven.Payment.Api;
+using System;
+using Java.Util;
+using System.Collections.Generic;
+
+namespace Sample
+{
+	[Activity]
+	public class ResultActivity : Activity
+	{
+
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+			SetContentView (Resource.Layout.Result);
+
+			TextView paymentResultView = FindViewById<TextView> (Resource.Id.payment_result);
+
+			Intent i = new Intent();
+			if (this.Intent.HasExtra("result")) {
+				i.PutExtras(this.Intent.Extras.GetBundle("result"));
+			}
+			int requestCode = this.Intent.GetIntExtra("request_code", 0);
+
+			PaylevenApi.HandleIntent (requestCode, i, new PaylevenCallback (paymentResultView));
+		}
+
+		class PaylevenCallback : Java.Lang.Object, IPaylevenResponseListener
+		{
+			TextView paymentDataView;
+
+			public PaylevenCallback (TextView paymentDataView)
+			{
+				this.paymentDataView = paymentDataView;
+			}
+
+			public void OnNoPaylevenResponse (Intent data)
+			{
+				
+			}
+
+			public void OnOpenSalesHistoryFinished (ApiSalesHistoryCompletedStatus status)
+			{
+
+			}
+
+			public void OnOpenTransactionDetailsFinished (string p0, IDictionary<string, string> p1, ApiTransactionDetailsCompletedStatus p2)
+			{
+
+			}
+
+			public void OnPaymentFinished (string orderId, TransactionRequest originalRequest, IDictionary<String, String> result, ApiPaymentCompletedStatus status)
+			{
+				//handle payment finished
+				string textResult = "Order ID: " + orderId + "\n" + "Status: " + status + "\n";
+
+
+				foreach(KeyValuePair<string, string> entry in result)
+				{
+					textResult = textResult + entry.Key + ": " + entry.Value + "\n";
+				}
+
+				this.paymentDataView.Text = textResult;
+			}
+		}
+	}
+}
+
